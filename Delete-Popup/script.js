d95f5738500415e01724fb077769e2a60c89675f
sunilMe <sanjayme571@gmail.com>
2026-02-15 11:55:35 +0530

    Fix folders - remove submodule issue

---
+++ Delete-Popup/script.js
@@ -0,0 +1,106 @@
+let checkbox = document.getElementById("checkbox");
+let paragraph = document.getElementById("paragraph");
+let popup = document.querySelector(".popup")
+let count = 1;
+
+function iconsAnimation() {
+  gsap.to("#delete", {
+    duration: 0.5,
+    top: -50,
+  })
+  gsap.to("#delete", {
+    duration: 0.5,
+    delay: 0.5,
+    top: 6,
+    zIndex: 5
+  })
+  gsap.to("#delete", {
+    duration: 0.5,
+    delay: 0.7,
+    zIndex: 1
+  })
+
+  setTimeout(() => {
+    checkbox.classList.remove("ri-checkbox-circle-line");
+    checkbox.classList.add("ri-checkbox-circle-fill");
+  }, 800)
+
+  gsap.to(checkbox, {
+    delay: 0.8,
+    duration: 0.1,
+    scale: 1.4
+  })
+}
+
+
+function createPopupFirecracker() {
+  const colors = ["#103783", "#432371", "#00e5ff", "#439cfb", "#42047e"];
+
+  for (let i = 0; i < 30; i++) {
+    const particle = document.createElement("div");
+    particle.classList.add("firework");
+
+    const angle = Math.random() * 2 * Math.PI;
+    const distance = Math.random() * 120;
+
+    const x = Math.cos(angle) * distance + "px";
+    const y = Math.sin(angle) * distance + "px";
+
+    particle.style.setProperty("--x", x);
+    particle.style.setProperty("--y", y);
+
+    particle.style.background =
+      colors[Math.floor(Math.random() * colors.length)];
+
+    popup.appendChild(particle);
+
+    setTimeout(() => {
+      particle.remove();
+    }, 700);
+  }
+
+}
+
+
+
+function removeAnimation() {
+  checkbox.classList.add("ri-checkbox-circle-line");
+  checkbox.classList.remove("ri-checkbox-circle-fill");
+  checkbox.style.transform = "scale(1)";
+  paragraph.textContent = "Are you sure you want to delete this item?"
+}
+
+function confirmDelete() {
+  iconsAnimation();
+
+  setTimeout(() => {
+    createPopupFirecracker();
+    paragraph.textContent = "Thank You!"
+  }, 700);
+  setTimeout(() => {
+   popup.style.visibility = "hidden"
+  }, 1800);
+  setTimeout(() => {
+    removeAnimation();
+  }, 1800);
+
+  count = 1
+}
+
+
+function closeDeletePopup() {
+  popup.style.visibility = "hidden";
+  count = 1;
+}
+
+
+function deletePopup() {
+  if (count === 1) {
+   popup.style.visibility = "visible";
+    count = 0;
+  } else {
+  popup.style.visibility = "hidden";
+    count = 1;
+  }
+}
+
